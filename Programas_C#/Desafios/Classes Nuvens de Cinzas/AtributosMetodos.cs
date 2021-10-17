c6e28ed1445e3d73fe160eec7d93367a78eeaa35
Gustavo Gomes <ggomes596@gmail.com>
2021-10-16 22:53:01 -0300

    Começo do Desafio

---
+++ Programas_C#/Desafios/Classes Nuvens de Cinzas/AtributosMetodos.cs
@@ -0,0 +1,10 @@
+namespace Desafios.Classes_Nuvens_de_Cinzas
+{
+    public class AtributosMetodos
+    {
+        //Atributos(ainda terá mais atributos)
+        public int numeroAeroportos;
+
+        //Metodos(ainda terá Metodos)
+    }
+}
--- Programas_C#/Desafios/Program.cs
+++ Programas_C#/Desafios/Program.cs
@@ -6,7 +6,14 @@ namespace Desafios
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Desafio I - Nuvens de Cinza");
+            Console.WriteLine("Informe o número de linhas e colunas respectivamente para a matriz");
+            var linhasColunas = Console.ReadLine().Split(' ');
+            var linhas = int.Parse(linhasColunas[0]);
+            var colunas = int.Parse(linhasColunas[1]);
+            Console.WriteLine(linhas+ " " + colunas);
+
+
         }
     }
 }

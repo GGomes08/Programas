ae3c51f32e3b04f2bf30de6e865a04bf1e6f7136
Gustavo Gomes <ggomes596@gmail.com>
2023-10-02 15:54:51 -0300

    Criação da Pasta e 2 projetos adicionados

---
+++ Programas_CSharp/Compactador/Compactador.cs
@@ -0,0 +1,134 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Programas_CSharp{
+
+    class Compactador{
+        public static void Main(string[] args){
+            var caminhoArq = "./";
+            var arqTexto = "textoDescompactado.txt";
+            var arqTextoSaida = "textoCompactado.txt";
+            var caminhoCompleto = caminhoArq + arqTexto;
+            var caminhoCompactado = caminhoArq + arqTextoSaida;
+
+            if (!Directory.Exists(caminhoArq))
+                File.Create(caminhoArq);
+
+            if (!File.Exists(caminhoCompleto))
+                File.Create(caminhoCompleto);
+
+            using (var sw = new StreamWriter(caminhoCompleto))
+            {
+                sw.WriteLine("Dear Sally,");
+                sw.WriteLine("Please, please do it--it would please");
+                sw.WriteLine("Mary very, very much. And Mary would");
+                sw.WriteLine("do everything in Mary's power to make");
+                sw.WriteLine("it pay off for you.");
+                sw.WriteLine("-- Thank you very much --");
+                sw.WriteLine("0");
+                sw.Close();
+            }
+
+            string[] vet = new string[100];
+            char letra;
+            int pos = 0;
+            int contPalavras = 0;
+            string palavra = "";
+            bool verifica = false;
+            StreamReader entrada = new StreamReader(caminhoCompleto);
+            StreamWriter saida = new StreamWriter(caminhoCompactado);
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("Seja Bem Vindo a Aplicação de compactar um arquivo texto");
+            Console.WriteLine("Iremos compactar o conteudo que se encontra no"
+                    + " arquivo texto nomeado como 'textoDescompactado'.");
+            do
+            {
+                letra = (char)entrada.Read();
+
+                if (Char.IsLetter(letra))
+                {
+                    palavra += letra;
+                }
+                else
+                {
+                    if (palavra.Length != 0)
+                    {
+                        pos = BuscaRecursiva(vet, palavra, 0, vet.Length - 1);
+                        if (pos == -1)
+                        {
+                            saida.Write(palavra);
+
+                        }
+                        else if (pos >= 0)
+                        {
+                            verifica = VerificaPal(vet, palavra, pos);
+                            if ((pos >= 0) && (verifica = true))
+                            {
+                                RemovePal(vet, pos);
+                                saida.Write(pos + 1 + "");
+
+                            }
+                        }
+
+                        InserePalavras(vet, palavra, contPalavras);
+                        palavra = "";
+                    }
+                    saida.Write(letra);
+
+                }
+
+            } while (letra != '0');
+            Console.WriteLine("---------------------------------------------------------------------------------");
+            Console.WriteLine("Por favor, verifique o 'textoCompactado.txt' localizado na pasta de documentos!");
+
+            entrada.Close();
+            saida.Close();
+        }
+        static int BuscaRecursiva(String[] vet, String palavra, int esq, int dir)
+        {
+            if (dir < esq)
+            {
+                return -1;
+            }
+            if (palavra.Equals(vet[esq]))
+            {
+                return esq;
+            }
+            return BuscaRecursiva(vet, palavra, esq + 1, dir);
+        }
+        //Metodo que insere as Palavras
+        static void InserePalavras(String[] vet, String x, int cont)
+        {
+            for (int i = vet.Length - 1; i > 0; i--)
+            {
+                if (vet[i - 1] != null)
+                {
+                    vet[i] = vet[i - 1];
+                    cont++;
+                }
+            }
+            vet[0] = x;
+        }
+
+        static void RemovePal(String[] vet, int a)
+        {
+            if (a >= 0 && a < vet.Length)
+            {
+                for (int i = a; i < vet.Length - 1; i++)
+                {
+                    vet[i] = vet[i + 1];
+                    vet[i + 1] = null;
+
+                }
+
+            }
+
+        }
+        static bool VerificaPal(String[] vet, String a, int b)
+        {
+            if (a.Equals(vet[b]))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Programas_CSharp/Fibonacci/Fibonacci.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Programas_CSharp
+{
+     class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.Write("Digite um termo de Fibonacci:");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite agora 'R'(Fibonacci Recursivo) ou 'I'(Fibonacci Iterativo):");
+            char letra = Convert.ToChar(Console.ReadLine());
+
+            if(letra == 'R' || letra=='r')
+            {
+            for(int i = 1; i<=numero; i++)
+            {
+            Console.WriteLine("Fibonacci Recursivo: "+ fiboRec(i));
+            }
+            }
+            else if(letra == 'I' || letra=='i')
+            {
+            for(int i = 1; i<=numero; i++)
+            {
+            Console.WriteLine("Fibonacci Iterativo: "+ fiboIte(i));
+            }
+            }
+        }
+
+        static int fiboRec(int numero)
+        {
+            if(numero > 1)
+            {
+                return fiboRec(numero-1)+ fiboRec(numero-2);
+            }
+            else
+            {
+                if(numero==1) return 1;
+                else return 0;
+            }
+        }
+
+        static int fiboIte(int numero)
+        {
+            int inicio=0;
+            int prox= 1;
+            int aux= 0;
+
+            for(int i= 1; i< numero; i++)
+            {
+                aux= inicio+prox;
+                inicio=prox;
+                prox= aux;
+            }
+            return aux;
+        }
+        
+    }
+}
